dc9ae993a85bcc4f101c33065a00b100c273cd28
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-06-22 16:41:47 +0300

    Generic Repository hazir ve  iş yapacak asıl katmanklar hazırlanıyor..

---
+++ eshopProject/eshop.DataAccessLayer/Concreate/EfCategoryDal.cs
@@ -0,0 +1,22 @@
+﻿using eshop.CoreLayer.DataAccess.Concreate;
+using eshop.DataAccessLayer.Abstract;
+using eshop.DataAccessLayer.DAL;
+using eshop.EntitiesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.DataAccessLayer.Concreate
+{
+    public class EfCategoryDal:GenericRepository<Category>,ICategoryDal
+    {
+        // private DatabaseContext GetContext { get { return _context as DatabaseContext; } }
+
+        public EfCategoryDal(DatabaseContext context):base(context)
+        {
+
+        }
+    }
+}
