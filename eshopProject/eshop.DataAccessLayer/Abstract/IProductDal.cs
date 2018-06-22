591ce6f0998dcb08f1cd6751421d8e283e1d8af3
dizdaroglu <furkndizdaroglu@gmail.com>
2018-06-22 17:12:36 +0300

    data access updated

---
+++ eshopProject/eshop.DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using eshop.CoreLayer.DataAccess.Abstract;
+using eshop.EntitiesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.DataAccessLayer.Abstract
+{
+   public  interface IProductDal:IGenericRepository<Product>
+    {
+    }
+}
