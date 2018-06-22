7d9c464258bf713cd2a2b168b7c1595acf8e890a
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-06-22 17:42:52 +0300

    unitOfWork Pattern created

---
+++ eshopProject/eshop.DataAccessLayer/IUnitOfWorkPattern/Abstract/IUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using eshop.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eshop.DataAccessLayer.IUnitOfWorkPattern.Abstract
+{
+    public interface IUnitOfWork
+    {
+
+        
+        int Complete();
+
+        // Get the interfaces
+        ICategoryDal CategoryDal { get; }
+        IBasketDal BasketDal { get; }
+        IBillAdressDal BillAdressDal { get; }
+        IBrandDal BrandDal { get; }
+        IColorsDal ColorsDal { get; }
+        ICommentsDal CommentsDal { get; }
+        ICredicartDal CredicartDal { get; }
+        ICustomerDal CustomerDal { get; }
+        IDiscountDal DiscountDal { get; }
+        IFavDal FavDal { get; }
+        IHomeSliderDal HomeSliderDal { get; }
+        IProductDal ProductDal { get; }
+        ISalesDal SalesDal { get; }
+        ISizeDal SizeDal { get; }
+        ISubCategoryDal SubCategoryDal { get; }
+        IImageFilesDal ImageFilesDal { get; }
+
+
+    }
+}
---
+++ eshopProject/eshop.Web/AutofacHelper/AutofacConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eshop.Web.AutofacHelper
+{
+    public class AutofacConfiguration
+    {
+        /// <summary>
+        /// Bu autofac ile config ayarlarımızı yapmaktadir.
+        /// </summary>
+        public static void Config()
+        {
+            // get the builder 
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            // cast işlemler 
+            //builder.RegisterType<>
+
+            IContainer container =  builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+
+        }
+    }
+}
